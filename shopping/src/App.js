69ce058aa5378f76680d9fb7c993b86d2c4c5713
heejinssss <heejin.dev33@gmail.com>
2023-07-21 20:06:52 +0900

    Feat: 상세페이지 이동

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
+import Detail from './pages/Detail.js';
 import { Routes, Route, Link, Router } from 'react-router-dom';
 
 function App() {
@@ -11,18 +12,15 @@ function App() {
   return (
     <div className="App">
 
-      <Routes>
-        <Route path="/" element={ <div>메인페이지</div> } />
-        <Route path="/detail" element={ <div>상세페이지</div> } />
-      </Routes>
-
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Shopping</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
+            {/* <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
@@ -31,7 +29,16 @@ function App() {
         style={{ backgroundImage : 'url(./bg.png)' }}>
       </div>
 
-      <div className="container">
+      <Routes>
+        <Route path="/" element={ <div>첫 화면입니다.</div> } />
+        <Route path="/detail" element={
+          <div>
+            <Detail />
+          </div>
+        } />
+      </Routes>
+
+      {/* <div className="container">
         <div className="row">
           { 
             shoes.map(function(shoes, i) {
@@ -43,20 +50,20 @@ function App() {
             })
           }
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
 
-function Card(props) {
-  return (
-    <div>
-      <img className="shoes" src={ props.shoes.url } style={{ width: "40%" }} alt="shoes" />
-      <h4>{ props.shoes.title }</h4>
-      <h5>{props.shoes.content }</h5>
-      <h5>{ props.shoes.price }</h5>
-    </div>
-  )
-}
+// function Card(props) {
+//   return (
+//     <div>
+//       <img className="shoes" src={ props.shoes.url } style={{ width: "40%" }} alt="shoes" />
+//       <h4>{ props.shoes.title }</h4>
+//       <h5>{props.shoes.content }</h5>
+//       <h5>{ props.shoes.price }</h5>
+//     </div>
+//   )
+// }
 
 export default App;
